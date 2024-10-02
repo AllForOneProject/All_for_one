cbc42b36ec381b12ab9136acd8af21c4a603e830
dudezi <a01036952858@gmail.com>
2024-10-02 12:03:46 +0900

    init

---
+++ src/main/java/com/market/AllForOne/AllForOneApplication.java
@@ -0,0 +1,13 @@
+package com.market.AllForOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllForOneApplication.class, args);
+	}
+
+}
